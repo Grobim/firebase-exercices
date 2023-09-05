09c1f4eccef268547d50b339052ee61a4cc17249
Nicolas 'Grobim' Chalet <nicolas.chalet@gmail.com>
2023-09-05 21:25:09 +0200

    added todolist

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -9,6 +9,7 @@ import ErrorPage from '../error-page';
 import Login from '../features/auth/Login';
 import Profile from '../features/auth/Profile';
 import BookFinder from '../features/book-finder/BookFinder';
+import TodoPage from '../features/todo/TodoPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
       <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/book-finder" element={<BookFinder />} />
+      <Route path="/todos" element={<TodoPage />} />
       <Route path="*" element={<ErrorPage message="Page not found" />} />
     </Route>,
   ),
